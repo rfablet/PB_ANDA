f19695486e823beab3082bdda56640a687eeca37
vietphi3892 <vietphi3892@gmail.com>
2017-08-31 10:41:15 +0200

    Update test_AnDA_SST.py

--- test_AnDA_SST.py
+++ test_AnDA_SST.py
@@ -21,6 +21,7 @@ np.random.seed(1)
 ###### Parameters setting for SST ###########################
 
 PR_sst = PR()
+PR_sst.flag_scale = True  # True: multi scale, False: one scale                    
 PR_sst.n = 50 # dimension state
 PR_sst.patch_r = 20 # size of patch
 PR_sst.patch_c = 20 # size of patch
@@ -29,33 +30,26 @@ PR_sst.test_days = 364 # num of test images: 2015
 PR_sst.lag = 3 # lag of time series: t -> t+lag
 PR_sst.G_PCA = 20 # N_eof for global PCA
 # Input dataset
-PR_sst.path_X = '/home/phi/test_global_scale/AnDA_github/data/AMSRE/sst.npz'
-PR_sst.path_OI = '/home/phi/test_global_scale/AnDA_github/data/AMSRE/OI.npz'
-PR_sst.path_mask = '/home/phi/test_global_scale/AnDA_github/data/AMSRE/metop_mask.npz'
+PR_sst.path_X = './sst.npz'
+PR_sst.path_OI = './OI.npz'
+PR_sst.path_mask = './metop_mask.npz'
 # Dataset automatically created during execution
-PR_sst.path_X_lr = '/home/phi/test_global_scale/AnDA_github/data/AMSRE/sst_lr_30.npz'
-PR_sst.path_dX_PCA = '/home/phi/test_global_scale/AnDA_github/data/AMSRE/dX_pca.npz'
-PR_sst.path_index_patches = '/home/phi/test_global_scale/AnDA_github/data/AMSRE/list_pos.pickle'
-PR_sst.path_neighbor_patches = '/home/phi/test_global_scale/AnDA_github/data/AMSRE/pair_pos.pickle'
+PR_sst.path_X_lr = './sst_lr_30.npz'
+PR_sst.path_dX_PCA = './dX_pca.npz'
+PR_sst.path_index_patches = './list_pos.pickle'
+PR_sst.path_neighbor_patches = './pair_pos.pickle'
 
 AF_sst = General_AF()
 AF_sst.flag_reduced =True # True: Reduced version of Local Linear AF
-AF_sst.flag_cond = True # True: use Obs at t+lag as condition to select successors
+AF_sst.flag_cond = False # True: use Obs at t+lag as condition to select successors
                   # False: no condition in analog forecasting
 AF_sst.flag_model = False # True: Use gradient, velocity as additional regressors in AF
 AF_sst.flag_catalog = True # True: each catalog for each patch position
                     # False: only one catalog for all positions
-AF_sst.flag_scale = True  # True: multi scale
-                    # False: one scale
-AF_sst.cluster = 10       # clusterized version AF
-AF_sst.k = 10 # number of analogs
-AF_sst.k_initial = 100 # retrieving k_initial nearest neighbors, then using condition to retrieve k analogs 
+AF_sst.cluster = 1       # clusterized version AF
+AF_sst.k = 200 # number of analogs
+AF_sst.k_initial = 200 # retrieving k_initial nearest neighbors, then using condition to retrieve k analogs 
 AF_sst.neighborhood = np.ones([PR_sst.n,PR_sst.n]) # global analogs
-AF_sst.neighborhood = np.eye(PR_sst.n)+np.diag(np.ones(PR_sst.n-1),1)+ np.diag(np.ones(PR_sst.n-1),-1)+ \
-                       np.diag(np.ones(PR_sst.n-2),2)+np.diag(np.ones(PR_sst.n-2),-2)
-AF_sst.neighborhood[0:2,:5] = 1
-AF_sst.neighborhood[PR_sst.n-2:,PR_sst.n-5:] = 1 # local analogs
-AF_sst.neighborhood[PR_sst.n-2:,PR_sst.n-5:] = 1 # local analogs
 AF_sst.regression = 'local_linear' 
 AF_sst.sampling = 'gaussian' 
 AF_sst.B = 0.01
@@ -65,7 +59,7 @@ AF_sst.R = 0.05
 VAR_sst = VAR()
 VAR_sst = Load_data(PR_sst) 
 
-file_tmp = np.load('/home/phi/test_global_scale/AnDA_github/data/AMSRE/DINEOF.npz')
+file_tmp = np.load('./DINEOF.npz')
 VAR_sst.dX_cond = file_tmp['sst_lr']
 del file_tmp    
 VAR_sst.dX_cond = VAR_sst.dX_cond[:PR_sst.test_days:PR_sst.lag,:,:]
@@ -79,19 +73,26 @@ r_start = 55
 c_start = 70
 r_length = 50
 c_length = 50
-AnDA_sst_3 = AnDA_result()
+level = 20 # 22 patches executed simultaneously
+
+saved_path =  'saved_path.pickle'
+AnDA_sst_1 = AnDA_result()
 MS_AnDA_sst = MS_AnDA(VAR_sst, PR_sst, AF_sst)
-AnDA_sst_3 = MS_AnDA_sst.multi_patches_assimilation('/home/phi/test_global_scale/AnDA_github/data/AMSRE/AnDA/AnDA_1.pickle', 9, r_start, r_length, c_start, c_length)
+AnDA_sst_1 = MS_AnDA_sst.multi_patches_assimilation(level, r_start, r_length, c_start, c_length)
 
 """ Postprocessing step: remove block artifact (do PCA twice gives perfect results) """
 Pre_filtered = np.copy(VAR_sst.dX_orig[:PR_sst.training_days,r_start:r_start+r_length,c_start:c_start+c_length]+VAR_sst.X_lr[:PR_sst.training_days,r_start:r_start+r_length,c_start:c_start+c_length])
-Pre_filtered = np.concatenate((Pre_filtered,AnDA_sst_3.itrp_AnDA),axis=0)
-AnDA_sst_3.itrp_postAnDA = Post_process(Pre_filtered,len(VAR_sst.Obs_test),33,150) 
-AnDA_sst_3.rmse_postAnDA = AnDA_RMSE(AnDA_sst_3.itrp_postAnDA,AnDA_sst_3.GT)
-AnDA_sst_3.corr_postAnDA = AnDA_correlate(AnDA_sst_3.itrp_postAnDA-AnDA_sst_3.LR,AnDA_sst_3.GT-AnDA_sst_3.LR)
+Pre_filtered = np.concatenate((Pre_filtered,AnDA_sst_1.itrp_AnDA),axis=0)
+AnDA_sst_1.itrp_postAnDA = Post_process(Pre_filtered,len(VAR_sst.Obs_test),33,150) 
+AnDA_sst_1.rmse_postAnDA = AnDA_RMSE(AnDA_sst_1.itrp_postAnDA,AnDA_sst_1.GT)
+AnDA_sst_1.corr_postAnDA = AnDA_correlate(AnDA_sst_1.itrp_postAnDA-AnDA_sst_1.LR,AnDA_sst_1.GT-AnDA_sst_1.LR)
 
+""" Save AnDA result """         
+with open(saved_path, 'wb') as handle:
+    pickle.dump(AnDA_sst_1, handle)
+    
 """  Reload saved AnDA result  """
-with open('/home/phi/test_global_scale/AnDA_github/data/AMSRE/AnDA/AnDA_1.pickle', 'rb') as handle:
+with open(saved_path, 'rb') as handle:
     AnDA_sst_1 = pickle.load(handle)    
 
 
